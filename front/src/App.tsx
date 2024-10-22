9ad5853170009fb90e0ab2733e36d9f64946c050
abhishek <abhiabhishektr@gmail.com>
2024-10-22 18:37:21 +0530

    frontend started

---
+++ front/src/App.tsx
@@ -0,0 +1,10 @@
+// App.jsx
+export default function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello
+      </h1>
+    </div>
+  );
+}
---
+++ front/tailwind.config.js
@@ -0,0 +1,10 @@
+// front/tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
